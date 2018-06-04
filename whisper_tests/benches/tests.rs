9b8f8f97ca4dd249c1e0ec3333f2a2dcb495ad9a
Deitrl <vboridko@googlemail.com>
2018-06-04 18:54:27 +0200

    add fetch and update benches

--- whisper_tests/benches/tests.rs
+++ whisper_tests/benches/tests.rs
@@ -2,15 +2,16 @@
 extern crate bencher;
 extern crate whisper;
 extern crate whisper_tests;
+extern crate rand;
 
 use bencher::Bencher;
-use std::path::PathBuf;
+use rand::*;
 use std::time::{SystemTime, UNIX_EPOCH};
 use whisper::WhisperMetadata;
 use whisper::retention::Retention;
-use whisper::archive_info::ArchiveInfo;
 use whisper::aggregation::AggregationMethod;
 use whisper_tests::*;
+use whisper::interval::Interval;
 
 
 fn create_metadata() -> WhisperMetadata {
@@ -24,17 +25,17 @@ fn create_metadata() -> WhisperMetadata {
 
 fn current_time() -> u32 {
     let since_epoch = SystemTime::now().duration_since(UNIX_EPOCH).expect("Time");
-    let millis: u32 = since_epoch.as_secs() as u32 * 1000 + since_epoch.subsec_nanos() as u32 / 1_000_000;
+    let millis: u32 = since_epoch.as_secs() as u32 * 1000 + since_epoch.subsec_nanos() / 1_000_000;
     millis
 }
 
 fn test_create(bench: &mut Bencher) {
-    let cleaner: CleanDir = CleanDir::new();
+    let mut cleaner: CleanTempDir = CleanTempDir::new();
     let meta = create_metadata();
     let mut index = 1;
     let i = &mut index;
     bench.iter(|| {
-        let path = PathBuf::from(format!("{}/whisper_{}.wsp", BENCH_FILE_PATH, i));
+        let path = cleaner.get_file_path("whisper_create", "wsp");
 
         whisper::create(&meta, &path, false).expect("creating");
         *i += 1;
@@ -42,29 +43,57 @@ fn test_create(bench: &mut Bencher) {
 }
 
 fn test_update(bench: &mut Bencher) {
-    let clean: CleanDir = CleanDir::new();
+    let mut cleaner: CleanTempDir = CleanTempDir::new();
     let meta = create_metadata();
-    let path = PathBuf::from(format!("{}/whisper_update_test.wsp", BENCH_FILE_PATH));
+    let path = cleaner.get_file_path("whisper_update", "wsp");
     whisper::create(&meta, &path, false).expect("Create file for update");
 
     let seconds_ago = 3500;
     let mut current_value = 0.5;
     let i = &mut current_value;
-    let mut index = 0;
-    let k = &mut index;
     let step = 0.2;
     let now = current_time();
 
     bench.iter(|| {
-        whisper::update(&path, *i, now - seconds_ago + *k, now as u32).expect("update");
-        *k += 1;
-        *i += step;
+        for j in 0..seconds_ago {
+            whisper::update(&path, *i, now - seconds_ago + j, now).expect("update");
+            *i += step;
+        }
     });
 }
 
+fn test_fetch(bench: &mut Bencher) {
+    let mut cleaner: CleanTempDir = CleanTempDir::new();
+    let meta = create_metadata();
+    let path = cleaner.get_file_path("whisper_fetch", "wsp");
+    whisper::create(&meta, &path, false).expect("Create file for fetching");
+
+    let seconds_ago = 3500;
+    let mut current_value = 0.5;
+    let i = &mut current_value;
+    let step = 0.2;
+    let now = current_time();
+
+    for j in 0..seconds_ago {
+        whisper::update(&path, *i, now - seconds_ago + j, now).expect("update");
+        *i += step;
+    };
+
+    let from_time = now - seconds_ago;
+    let until_time = from_time + 1000;
+    let interval = Interval::new(from_time, until_time).expect("interval");
+    bench.iter(|| {
+        // TODO: seconds per point is not in python api
+        whisper::fetch(&path, interval, now, rand::thread_rng().next_u32());
+    });
+}
+
+
+
 benchmark_group!(
 benches,
-test_create,
-//test_update
+//test_create,
+//test_update,
+test_fetch,
 );
 benchmark_main!(benches);
--- whisper_tests/src/lib.rs
+++ whisper_tests/src/lib.rs
@@ -1,19 +1,37 @@
-use std::fs;
-use std::panic::*;
+extern crate rand;
 
-pub const BENCH_FILE_PATH: &'static str = "/tmp/whisper";
+use std::fs::*;
+use std::path::PathBuf;
+use rand::*;
 
-pub struct CleanDir;
+#[derive(Debug)]
+pub struct CleanTempDir {
+    base_path: String,
+    files: Vec<String>,
+}
 
-impl CleanDir {
+impl CleanTempDir {
     pub fn new() -> Self {
-        fs::create_dir_all(BENCH_FILE_PATH).expect("Pre-creating dir");
-        CleanDir
+        create_dir_all("/tmp/whisper").expect("Pre-creating dir");
+        CleanTempDir { base_path: String::from("/tmp/whisper"), files: vec![] }
+    }
+
+    pub fn get_file_path(&mut self, prefix: &str, extension: &str) -> PathBuf {
+        let file_name = format!("{}/{}_{}.{}", self.base_path, prefix, random_string_suffix(), extension);
+        self.files.push(file_name.clone());
+        PathBuf::from(file_name)
     }
 }
 
-impl Drop for CleanDir {
+impl Drop for CleanTempDir {
     fn drop(&mut self) {
-         fs::remove_dir_all(BENCH_FILE_PATH).expect("Clean dir");
+        self.files.iter().map(|f| remove_file(f).expect("remove file"));
     }
+}
+
+fn random_string_suffix() -> String {
+    rand::thread_rng()
+        .gen_ascii_chars()
+        .take(10)
+        .collect::<String>()
 }
