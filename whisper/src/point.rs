7672fe459a869f3bcfbf31cc4576604e0334ca4e
Andrii Radyk <ander.ender@gmail.com>
2018-06-05 11:20:48 +0200

    add conversion from string for Point

--- whisper/src/point.rs
+++ whisper/src/point.rs
@@ -1,5 +1,7 @@
 use std::io;
 use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt};
+use std::str::FromStr;
+use failure::Error;
 
 #[derive(Debug, Clone, Copy)]
 pub struct Point {
@@ -28,5 +30,23 @@ impl Point {
         write.write_u32::<BigEndian>(self.interval)?;
         write.write_f64::<BigEndian>(self.value)?;
         Ok(())
+    }
+}
+
+impl FromStr for Point {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Point, Error> {
+        let segments: Vec<&str> = s.split(":").collect();
+
+        let (interval, value) = match segments.len() {
+            2 => (segments[0], segments[1]),
+            _ => return Err(format_err!("Cannot parse point from string: {}", s)),
+        };
+
+        Ok(Point{
+            interval: interval.parse()?,
+            value: value.parse()?,
+        })
     }
 }
