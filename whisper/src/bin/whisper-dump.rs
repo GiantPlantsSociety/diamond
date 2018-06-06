a49b15ef90bef167bff315327b76cbf3e452f753
Andrii Radyk <ander.ender@gmail.com>
2018-06-06 21:14:50 +0200

    cleanup

--- whisper/src/bin/whisper-dump.rs
+++ whisper/src/bin/whisper-dump.rs
@@ -17,7 +17,7 @@ struct Args {
     #[structopt(long = "pretty", requires = "time_format")]
     pretty: bool,
 
-    /// Time format to use with --pretty; see time.strftime()
+    /// Time format to use with --pretty; see https://docs.rs/chrono/0.4.0/chrono/format/strftime/index.html
     #[structopt(long = "time-format", short = "t", requires = "pretty")]
     time_format: Option<String>,
 
@@ -25,41 +25,6 @@ struct Args {
     #[structopt(name = "path", parse(from_os_str))]
     path: PathBuf,
 }
-
-// whisper-dump.py
-// Usage: whisper-dump.py path
-//
-// Options:
-//   -h, --help            show this help message and exit
-//   --pretty              Show human-readable timestamps instead of unix times
-//   -t TIME_FORMAT, --time-format=TIME_FORMAT
-//                         Time format to use with --pretty; see time.strftime()
-
-// whisper-create.py load.3m.wsp 15m:8
-// Created: load.3m.wsp (124 bytes)
-
-// whisper-dump.py load.3m.wsp
-// Meta data:
-//   aggregation method: average
-//   max retention: 7200
-//   xFilesFactor: 0.5
-
-// Archive 0 info:
-//   offset: 28
-//   seconds per point: 900
-//   points: 8
-//   retention: 7200
-//   size: 96
-
-// Archive 0 data:
-// 0: 0,          0
-// 1: 0,          0
-// 2: 0,          0
-// 3: 0,          0
-// 4: 0,          0
-// 5: 0,          0
-// 6: 0,          0
-// 7: 0,          0
 
 fn main() -> Result<(), Error> {
     let args = Args::from_args();
