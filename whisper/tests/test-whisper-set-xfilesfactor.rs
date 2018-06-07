375b1738d639b6f2187616e4f990eae9e5131368
Andrii Radyk <ander.ender@gmail.com>
2018-06-07 21:09:03 +0200

    add whisper-set-xfilesfactor

---
+++ whisper/tests/test-whisper-set-xfilesfactor.rs
@@ -0,0 +1,24 @@
+extern crate assert_cli;
+
+#[cfg(test)]
+mod whisper_set_xfilesfactor {
+    use assert_cli;
+
+    const NAME: &str = "whisper-set-xfilesfactor";
+
+    #[test]
+    fn calling_without_args() {
+        assert_cli::Assert::cargo_binary(NAME)
+            .fails_with(1)
+            .stderr().contains("USAGE")
+            .unwrap();
+    }
+
+    #[test]
+    fn calling_help() {
+        assert_cli::Assert::cargo_binary(NAME)
+            .with_args(&["--help"])
+            .stdout().contains("USAGE")
+            .unwrap();
+    }
+}
