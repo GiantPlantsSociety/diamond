33f5cb14402124dafe47c68f47db44e78781eb0d
Andrey Kutejko <andy128k@gmail.com>
2018-06-11 21:13:16 +0200

    Update test-whisper-update.rs

--- whisper_tests/tests/test-whisper-update.rs
+++ whisper_tests/tests/test-whisper-update.rs
@@ -9,7 +9,7 @@ mod whisper_update {
 
     #[test]
     fn calling_without_args() {
-       get_binary_command(NAME)
+        get_binary_command(NAME)
             .fails_with(1)
             .stderr().contains("USAGE")
             .unwrap();
@@ -17,7 +17,7 @@ mod whisper_update {
 
     #[test]
     fn calling_help() {
-       get_binary_command(NAME)
+        get_binary_command(NAME)
             .with_args(&["--help"])
             .stdout().contains("USAGE")
             .unwrap();
@@ -25,7 +25,7 @@ mod whisper_update {
 
     #[test]
     fn calling_with_invalid_path() {
-       get_binary_command(NAME)
+        get_binary_command(NAME)
             .with_args(&["invalid", "1:1"])
             .fails_with(1)
             .unwrap();
@@ -33,7 +33,7 @@ mod whisper_update {
 
     #[test]
     fn calling_with_invalid_timestamp() {
-       get_binary_command(NAME)
+        get_binary_command(NAME)
             .with_args(&["some", "nottimestamp:1"])
             .fails_with(1)
             .unwrap();
@@ -41,10 +41,9 @@ mod whisper_update {
 
     #[test]
     fn calling_with_invalid_value() {
-       get_binary_command(NAME)
+        get_binary_command(NAME)
             .with_args(&["some", "1:value"])
             .fails_with(1)
             .unwrap();
     }
-
 }
