ad91d307dd695a50af102c62fc936c30ccbbe17b
Deitrl <vboridko@googlemail.com>
2018-06-11 20:09:54 +0200

    Move tests to whisper_tests crate

--- whisper_tests/tests/test-find-corrupt-whisper-files.rs
+++ whisper_tests/tests/test-find-corrupt-whisper-files.rs
@@ -1,14 +1,15 @@
 extern crate assert_cli;
+extern crate whisper_tests;
 
 #[cfg(test)]
-mod whisper_dump {
-    use assert_cli;
+mod find_corrupt_whisper_files {
+    use whisper_tests::*;
 
-    const NAME: &str = "whisper-dump";
+    const NAME: &str = "find-corrupt-whisper-files";
 
     #[test]
     fn calling_without_args() {
-        assert_cli::Assert::cargo_binary(NAME)
+        get_binary_command(NAME)
             .fails_with(1)
             .stderr().contains("USAGE")
             .unwrap();
@@ -16,7 +17,7 @@ mod whisper_dump {
 
     #[test]
     fn calling_help() {
-        assert_cli::Assert::cargo_binary(NAME)
+        get_binary_command(NAME)
             .with_args(&["--help"])
             .stdout().contains("USAGE")
             .unwrap();
@@ -24,7 +25,7 @@ mod whisper_dump {
 
     #[test]
     fn calling_with_invalid_path() {
-        assert_cli::Assert::cargo_binary(NAME)
+        get_binary_command(NAME)
             .with_args(&["invalid"])
             .fails_with(1)
             .unwrap();
