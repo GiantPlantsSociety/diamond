7f06e6958c7fe21d0736ab7108d5ed272f04deb8
Andrii Radyk <ander.ender@gmail.com>
2018-06-15 20:32:54 +0200

    move to iterative style

--- whisper/src/bin/find-corrupt-whisper-files.rs
+++ whisper/src/bin/find-corrupt-whisper-files.rs
@@ -6,11 +6,10 @@ extern crate whisper;
 
 use failure::Error;
 use std::fs::remove_file;
-use std::path::{PathBuf, Path};
+use std::path::{Path, PathBuf};
 use std::process::exit;
 use structopt::StructOpt;
 use walkdir::WalkDir;
-
 
 #[derive(Debug, StructOpt)]
 #[structopt(
@@ -40,32 +39,30 @@ fn is_whisper_file(path: &Path) -> bool {
     path.extension() == Some(std::ffi::OsStr::new("wsp"))
 }
 
-fn collect_whisper_files(dir: &Path) -> Vec<PathBuf> {
+fn walk_dir(dir: &Path, delete_corrupt: bool) {
     WalkDir::new(dir)
         .min_depth(1)
         .into_iter()
         .filter_map(|entry| match entry {
             Ok(ref entry) if is_whisper_file(entry.path()) => Some(entry.path().to_path_buf()),
-            _ => None
+            _ => None,
         })
-        .collect::<Vec<PathBuf>>()
+        .map(|file| delete_corrupt_file(&file, delete_corrupt))
+        .collect()
 }
 
-fn walk_dir(dir: &Path, delete_corrupt: bool) {
-    let whisper_files = collect_whisper_files(dir);
-    for file in &whisper_files {
-        match whisper::WhisperFile::open(file) {
-            Ok(whisper_file) => {
-                let x: u32 = whisper_file.info().archives.iter().map(|a| a.points).sum();
-                println!("{:?}: {} points", file, x);
-            }
-            _ => {
-                if delete_corrupt {
-                    eprintln!("Deleting corrupt Whisper file: {:?}", file);
-                    remove_file(file).expect("Deleting File");
-                } else {
-                    eprintln!("Corrupt Whisper file: {:?}", file);
-                }
+fn delete_corrupt_file(file: &Path, delete_corrupt: bool) {
+    match whisper::WhisperFile::open(file) {
+        Ok(whisper_file) => {
+            let x: u32 = whisper_file.info().archives.iter().map(|a| a.points).sum();
+            println!("{:?}: {} points", file, x);
+        }
+        _ => {
+            if delete_corrupt {
+                eprintln!("Deleting corrupt Whisper file: {:?}", file);
+                remove_file(file).expect("Deleting File");
+            } else {
+                eprintln!("Corrupt Whisper file: {:?}", file);
             }
         }
     }
