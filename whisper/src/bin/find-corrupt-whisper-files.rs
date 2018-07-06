2ffb6eb068654cb5090464af62e4e88eb7b2f75e
Andrii Radyk <ander.ender@gmail.com>
2018-07-06 09:21:36 +0200

    Refactoring find corrupt

--- whisper/src/bin/find-corrupt-whisper-files.rs
+++ whisper/src/bin/find-corrupt-whisper-files.rs
@@ -11,27 +11,20 @@ use std::process::exit;
 use structopt::StructOpt;
 use walkdir::WalkDir;
 
+/// Find and (optionally) delete corrupt Whisper data files.
 #[derive(Debug, StructOpt)]
-#[structopt(
-    name = "find-corrupt-whisper-files",
-    about = "Find and (optionally) delete corrupt Whisper data files."
-)]
+#[structopt(name = "find-corrupt-whisper-files")]
 struct Args {
     /// Delete reported files.
-    #[structopt(long = "delete-corrupt", help = "Delete reported files")]
+    #[structopt(long = "delete-corrupt")]
     delete_corrupt: bool,
 
     /// Display progress info.
-    #[structopt(long = "verbose", help = "Display progress info")]
+    #[structopt(long = "verbose")]
     verbose: bool,
 
     /// Directory containing Whisper files.
-    #[structopt(
-        name = "WHISPER_DIR",
-        help = "Directory containing Whisper files",
-        parse(from_os_str),
-        raw(required = "true", min_values = "1")
-    )]
+    #[structopt(name = "WHISPER_DIR", parse(from_os_str), raw(required = "true", min_values = "1"))]
     directories: Vec<PathBuf>,
 }
 
@@ -39,50 +32,64 @@ fn is_whisper_file(path: &Path) -> bool {
     path.extension() == Some(std::ffi::OsStr::new("wsp"))
 }
 
-fn walk_dir(dir: &Path, delete_corrupt: bool) {
-    WalkDir::new(dir)
-        .min_depth(1)
-        .into_iter()
-        .filter_map(|entry| match entry {
-            Ok(ref entry) if is_whisper_file(entry.path()) => Some(entry.path().to_path_buf()),
-            _ => None,
-        })
-        .map(|file| delete_corrupt_file(&file, delete_corrupt))
-        .collect()
+fn walk_dir(dir: &Path, delete_corrupt: bool, verbose: bool) -> Result<(), Error> {
+    for entry in WalkDir::new(dir).min_depth(1).into_iter() {
+        match entry {
+            Ok(ref entry) if verbose && entry.file_type().is_dir() => {
+                println!("Scanning {}...", entry.path().canonicalize()?.display())
+            }
+            Ok(ref entry) if is_whisper_file(entry.path()) => {
+                delete_corrupt_file(&entry.path(), delete_corrupt)?
+            }
+            Err(e) => {
+                eprintln!("{}", e)
+            }
+            _ => {}
+        }
+    }
+
+    Ok(())
 }
 
-fn delete_corrupt_file(file: &Path, delete_corrupt: bool) {
+fn delete_corrupt_file(file: &Path, delete_corrupt: bool) -> Result<(), Error> {
     match whisper::WhisperFile::open(file) {
         Ok(whisper_file) => {
             let x: u32 = whisper_file.info().archives.iter().map(|a| a.points).sum();
-            println!("{:?}: {} points", file, x);
+            println!("{}: {} points", file.canonicalize()?.display(), x);
         }
         _ => {
             if delete_corrupt {
-                eprintln!("Deleting corrupt Whisper file: {:?}", file);
-                remove_file(file).expect("Deleting File");
+                eprintln!("Deleting corrupt Whisper file: {}", file.canonicalize()?.display());
+                remove_file(file)?;
             } else {
-                eprintln!("Corrupt Whisper file: {:?}", file);
+                eprintln!("Corrupt Whisper file: {}", file.canonicalize()?.display());
             }
         }
     }
+    Ok(())
 }
 
-fn main() -> Result<(), Error> {
-    let args = Args::from_args();
-
+fn run(args: &Args) -> Result<(), Error> {
     for dir in &args.directories {
         if !dir.is_dir() {
-            eprintln!("{:?} is not a directory or not exist!", dir);
+            eprintln!("{} is not a directory or not exist!", dir.display());
             exit(1);
         }
 
         if args.verbose {
-            println!("Scanning {:?}...", dir);
+            println!("Scanning {}...", dir.canonicalize()?.display());
         }
 
-        walk_dir(dir, args.delete_corrupt);
+        walk_dir(dir, args.delete_corrupt, args.verbose)?;
     }
 
     Ok(())
+}
+
+fn main() {
+    let args = Args::from_args();
+    if let Err(err) = run(&args) {
+        eprintln!("{}", err);
+        exit(1);
+    }
 }
