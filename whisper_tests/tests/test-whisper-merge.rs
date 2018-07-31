66c95088e08d4521966347ff169fc238567a697e
Andrii Radyk <ander.ender@gmail.com>
2018-07-31 23:12:29 +0200

    add test: merge with overlaping intervals

--- whisper_tests/tests/test-whisper-merge.rs
+++ whisper_tests/tests/test-whisper-merge.rs
@@ -45,8 +45,22 @@ fn create_and_update_many(path: &PathBuf, timestamps: &[u32], now: u32) -> Whisp
     file
 }
 
+fn create_and_update_points(path: &PathBuf, points: &[Point], now: u32) -> WhisperFile {
+    let mut file = WhisperBuilder::default()
+        .add_retention(Retention {
+            seconds_per_point: 60,
+            points: 10,
+        })
+        .build(path)
+        .unwrap();
+
+    file.update_many(&points, now).unwrap();
+
+    file
+}
+
 #[test]
-fn issue34_merge_update() {
+fn test_merge_update() {
     let temp_dir = get_temp_dir();
 
     let path1 = get_file_path(&temp_dir, "issue34_1");
@@ -78,7 +92,7 @@ fn issue34_merge_update() {
 }
 
 #[test]
-fn issue34_merge_update_many() {
+fn test_merge_update_many() {
     let temp_dir = get_temp_dir();
 
     let path1 = get_file_path(&temp_dir, "issue34_3");
@@ -105,6 +119,87 @@ fn issue34_merge_update_many() {
             points.iter().any(|p| p.interval == now - delta),
             "should contain (now - {}) from file2",
             delta
+        );
+    }
+}
+
+#[test]
+fn test_merge_overwrite() {
+    let temp_dir = get_temp_dir();
+
+    let path1 = get_file_path(&temp_dir, "issue54_1");
+    let path2 = get_file_path(&temp_dir, "issue54_2");
+
+    let now = 1528240800;
+
+    let mut _file1 = create_and_update_points(
+        &path1,
+        &[
+            Point {
+                interval: now - 60,
+                value: 60.0,
+            },
+            Point {
+                interval: now - 180,
+                value: 180.0,
+            },
+            Point {
+                interval: now - 300,
+                value: 300.0,
+            },
+        ],
+        now,
+    );
+
+    let mut file2 = create_and_update_points(
+        &path2,
+        &[
+            Point {
+                interval: now - 120,
+                value: 120.0,
+            },
+            Point {
+                interval: now - 300,
+                value: 3000.0,
+            },
+            Point {
+                interval: now - 360,
+                value: 360.0,
+            },
+            Point {
+                interval: now - 480,
+                value: 480.0,
+            },
+        ],
+        now,
+    );
+
+    whisper::merge::merge(&path1, &path2, 0, now, now).unwrap();
+    let points = file2.dump(60).unwrap();
+
+    for delta in &[60, 180, 300] {
+        assert!(
+            points
+                .iter()
+                .any(|p| p.interval == (now - delta) && p.value == f64::from(*delta)),
+            "should contain (now - {} = {}, {}) from file1: {:?}",
+            delta,
+            now - delta,
+            delta,
+            points
+        );
+    }
+
+    for delta in &[120, 360, 480] {
+        assert!(
+            points
+                .iter()
+                .any(|p| p.interval == (now - delta) && p.value == f64::from(*delta)),
+            "should contain (now - {} = {}, {}) from file2, points: {:?}",
+            delta,
+            now - delta,
+            delta,
+            points
         );
     }
 }
