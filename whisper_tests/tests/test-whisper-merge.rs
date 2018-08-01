a64bfeb9a0da3713671b1f0b63f36a772c489231
Andrii Radyk <ander.ender@gmail.com>
2018-08-01 17:06:08 +0200

    add test for fill

--- whisper_tests/tests/test-whisper-merge.rs
+++ whisper_tests/tests/test-whisper-merge.rs
@@ -203,3 +203,84 @@ fn test_merge_overwrite() {
         );
     }
 }
+
+#[test]
+fn test_fill_overlap() {
+    let temp_dir = get_temp_dir();
+
+    let path1 = get_file_path(&temp_dir, "issue54_1");
+    let path2 = get_file_path(&temp_dir, "issue54_2");
+
+    let now = 1528240800;
+
+    let mut _file1 = create_and_update_points(
+        &path1,
+        &[
+            Point {
+                interval: now - 60,
+                value: 60.0,
+            },
+            Point {
+                interval: now - 180,
+                value: 180.0,
+            },
+            Point {
+                interval: now - 300,
+                value: 3000.0,
+            },
+        ],
+        now,
+    );
+
+    let mut file2 = create_and_update_points(
+        &path2,
+        &[
+            Point {
+                interval: now - 120,
+                value: 120.0,
+            },
+            Point {
+                interval: now - 300,
+                value: 300.0,
+            },
+            Point {
+                interval: now - 360,
+                value: 360.0,
+            },
+            Point {
+                interval: now - 480,
+                value: 480.0,
+            },
+        ],
+        now,
+    );
+
+    whisper::fill::fill(&path1, &path2, now, now).unwrap();
+    let points = file2.dump(60).unwrap();
+
+    for delta in &[180] {
+        assert!(
+            points
+                .iter()
+                .any(|p| p.interval == (now - delta) && p.value == f64::from(*delta)),
+            "should contain (now - {} = {}, {}) from file1: {:?}",
+            delta,
+            now - delta,
+            delta,
+            points
+        );
+    }
+
+    for delta in &[120, 300, 360, 480] {
+        assert!(
+            points
+                .iter()
+                .any(|p| p.interval == (now - delta) && p.value == f64::from(*delta)),
+            "should contain (now - {} = {}, {}) from file2, points: {:?}",
+            delta,
+            now - delta,
+            delta,
+            points
+        );
+    }
+}
