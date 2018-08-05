bfd60d133e309ef8489b469fa8375e54afbcfd15
Andrii Radyk <ander.ender@gmail.com>
2018-08-06 01:52:06 +0200

    WIP

--- carbon/src/bin/carbon-pipe.rs
+++ carbon/src/bin/carbon-pipe.rs
@@ -1,3 +1,5 @@
+#[macro_use]
+extern crate structopt;
 extern crate carbon;
 extern crate failure;
 extern crate whisper;
@@ -9,24 +11,52 @@ use std::io;
 use std::io::BufRead;
 use std::path::PathBuf;
 use std::time::{SystemTime, UNIX_EPOCH};
+use structopt::StructOpt;
+use whisper::aggregation::AggregationMethod;
 use whisper::builder::WhisperBuilder;
 use whisper::retention::Retention;
 use whisper::WhisperFile;
 
-fn main() -> Result<(), Error> {
-    let stdin = io::stdin();
-    let _dir = '.';
+/// Receive metrics from pipe
+#[derive(Debug, StructOpt)]
+#[structopt(name = "carbon-pipe")]
+struct Args {
+    /// Path to data file
+    #[structopt(name = "path", parse(from_os_str))]
+    path: PathBuf,
 
-    let retention = Retention {
-        seconds_per_point: 60,
-        points: 10,
-    };
+    /// XFILESFACTOR
+    #[structopt(long = "xFilesFactor", default_value = "0.5")]
+    x_files_factor: f32,
+
+    /// Function to use when aggregating values
+    /// (average, sum, last, max, min, avg_zero, absmax, absmin)
+    #[structopt(long = "aggregationMethod", default_value = "average")]
+    aggregation_method: AggregationMethod,
+
+    #[structopt(
+        name = "retentions",
+        help = r#"Specify lengths of time, for example:
+60:1440      60 seconds per datapoint, 1440 datapoints = 1 day of retention
+15m:8        15 minutes per datapoint, 8 datapoints = 2 hours of retention
+1h:7d        1 hour per datapoint, 7 days of retention
+12h:2y       12 hours per datapoint, 2 years of retention
+"#,
+        raw(required = "true", min_values = "1")
+    )]
+    retentions: Vec<Retention>,
+}
+
+fn main() -> Result<(), Error> {
+    let args = Args::from_args();
+    let stdin = io::stdin();
 
     for line in stdin.lock().lines() {
         let metric: MetricPoint = line?.parse()?;
         let metric_path: MetricPath = metric.name.parse()?;
 
-        let file_path: PathBuf = metric_path.into();
+        let mut file_path: PathBuf = args.path.clone();
+        file_path.push(metric_path.0);
 
         let mut file = match file_path.exists() {
             false => {
@@ -34,7 +64,9 @@ fn main() -> Result<(), Error> {
                 fs::create_dir_all(&dir_path)?;
 
                 WhisperBuilder::default()
-                    .add_retention(retention.clone())
+                    .add_retentions(&args.retentions)
+                    .x_files_factor(args.x_files_factor)
+                    .aggregation_method(args.aggregation_method)
                     .build(&file_path)
                     .unwrap()
             }
