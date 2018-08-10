63f8f4f6720f0be88ebbada3b1bbf56aafe1c4c8
Andrii Radyk <ander.ender@gmail.com>
2018-08-10 17:56:45 +0200

    skeleton carbon-pipe

---
+++ carbon/src/bin/carbon-pipe.rs
@@ -0,0 +1,88 @@
+#[macro_use]
+extern crate structopt;
+extern crate carbon;
+extern crate failure;
+extern crate whisper;
+
+use carbon::{MetricPath, MetricPoint};
+use failure::Error;
+use std::fs;
+use std::io;
+use std::io::BufRead;
+use std::path::PathBuf;
+use std::process::exit;
+use std::time::{SystemTime, UNIX_EPOCH};
+use structopt::StructOpt;
+use whisper::aggregation::AggregationMethod;
+use whisper::builder::WhisperBuilder;
+use whisper::retention::Retention;
+use whisper::WhisperFile;
+
+/// Receive metrics from pipe
+#[derive(Debug, StructOpt)]
+#[structopt(name = "carbon-pipe")]
+struct Args {
+    /// Path to data file
+    #[structopt(name = "path", parse(from_os_str))]
+    path: PathBuf,
+
+    /// XFILESFACTOR
+    #[structopt(long = "xFilesFactor", default_value = "0.5")]
+    x_files_factor: f32,
+
+    /// Function to use when aggregating values
+    /// (average, sum, last, max, min, avg_zero, absmax, absmin)
+    #[structopt(long = "aggregationMethod", default_value = "average")]
+    aggregation_method: AggregationMethod,
+
+    #[structopt(
+        name = "retentions",
+        help = r#"Specify lengths of time, for example:
+60:1440      60 seconds per datapoint, 1440 datapoints = 1 day of retention
+15m:8        15 minutes per datapoint, 8 datapoints = 2 hours of retention
+1h:7d        1 hour per datapoint, 7 days of retention
+12h:2y       12 hours per datapoint, 2 years of retention
+"#,
+        raw(required = "true", min_values = "1")
+    )]
+    retentions: Vec<Retention>,
+}
+
+fn run(args: &Args) -> Result<(), Error> {
+    let stdin = io::stdin();
+
+    for line in stdin.lock().lines() {
+        let metric: MetricPoint = line?.parse()?;
+        let metric_path: MetricPath = metric.name.parse()?;
+
+        let mut file_path: PathBuf = args.path.clone();
+        file_path.push(metric_path.0);
+
+        let mut file = match file_path.exists() {
+            false => {
+                let dir_path = file_path.parent().unwrap();
+                fs::create_dir_all(&dir_path)?;
+
+                WhisperBuilder::default()
+                    .add_retentions(&args.retentions)
+                    .x_files_factor(args.x_files_factor)
+                    .aggregation_method(args.aggregation_method)
+                    .build(&file_path)
+                    .unwrap()
+            }
+            true => WhisperFile::open(&file_path)?,
+        };
+
+        let now = SystemTime::now().duration_since(UNIX_EPOCH)?.as_secs() as u32;
+        file.update(&metric.point, now)?;
+    }
+    Ok(())
+}
+
+fn main() {
+    let args = Args::from_args();
+    if let Err(err) = run(&args) {
+        eprintln!("{}", err);
+        exit(1);
+    }
+}
---
+++ carbon/src/lib.rs
@@ -0,0 +1,205 @@
+#[macro_use]
+extern crate failure;
+#[macro_use]
+extern crate lazy_static;
+extern crate regex;
+extern crate whisper;
+
+use failure::Error;
+use regex::Regex;
+use std::convert::From;
+use std::path::PathBuf;
+use std::str::FromStr;
+use whisper::point::Point;
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct MetricPoint {
+    pub name: String,
+    pub point: Point,
+}
+
+#[derive(Debug, Clone)]
+pub struct MetricPoints {
+    pub name: String,
+    pub points: Vec<Point>,
+}
+
+#[derive(Fail, Debug)]
+enum MetricError {
+    #[fail(display = "Metric line({}) can not be validated", _0)]
+    ValidateError(String),
+    #[fail(display = "Metric name({}) can not be validated", _0)]
+    NameValidateError(String),
+    #[fail(display = "Can not parse metric from line: {}", _0)]
+    MetricLineParseError(String),
+}
+
+impl MetricPoint {
+    fn validate(s: &str) -> Result<(), MetricError> {
+        lazy_static! {
+            static ref RE: Regex = Regex::new(r"^[\d\w\._ -]+$").unwrap();
+        }
+
+        if RE.is_match(s) {
+            Ok(())
+        } else {
+            Err(MetricError::ValidateError(s.to_owned()))
+        }
+    }
+}
+
+impl FromStr for MetricPoint {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<MetricPoint, Error> {
+        MetricPoint::validate(s)?;
+
+        let segments: Vec<&str> = s.split(' ').collect();
+
+        let (name, timestamp, value) = match segments.len() {
+            3 => (segments[0], segments[1], segments[2]),
+            _ => return Err(MetricError::MetricLineParseError(s.to_owned()).into()),
+        };
+
+        Ok(MetricPoint {
+            name: name.to_owned(),
+            point: Point {
+                interval: timestamp.parse()?,
+                value: value.parse()?,
+            },
+        })
+    }
+}
+
+impl From<MetricPoints> for Vec<MetricPoint> {
+    fn from(m: MetricPoints) -> Vec<MetricPoint> {
+        let mut vector: Vec<MetricPoint> = Vec::new();
+        let name = m.name;
+        for point in m.points.iter() {
+            vector.push(MetricPoint {
+                name: name.to_owned(),
+                point: point.to_owned(),
+            });
+        }
+        vector
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct MetricPath(pub PathBuf);
+
+impl MetricPath {
+    fn validate(s: &str) -> Result<(), MetricError> {
+        lazy_static! {
+            static ref RE: Regex = Regex::new(r"^[\d\w\._-]+$").unwrap();
+        }
+
+        if RE.is_match(s) {
+            Ok(())
+        } else {
+            Err(MetricError::NameValidateError(s.to_owned()))
+        }
+    }
+}
+
+impl FromStr for MetricPath {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<MetricPath, Error> {
+        MetricPath::validate(s)?;
+        let segments: Vec<&str> = s.split('.').collect();
+
+        let path = segments
+            .iter()
+            .fold(PathBuf::new().join("."), |acc, x| acc.join(x))
+            .with_extension("wsp");
+
+        Ok(MetricPath(path))
+    }
+}
+
+impl From<MetricPath> for PathBuf {
+    fn from(metric_path: MetricPath) -> PathBuf {
+        metric_path.0
+    }
+}
+
+impl From<PathBuf> for MetricPath {
+    fn from(metric_path: PathBuf) -> MetricPath {
+        MetricPath(metric_path)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::path::Path;
+
+    #[test]
+    fn test_metric_correct_parse() {
+        let metric_result = "this.is.correct 1 123".parse::<MetricPoint>();
+        assert!(metric_result.is_ok(), "It should be parsed");
+
+        let metric = metric_result.unwrap();
+        assert_eq!(
+            metric,
+            MetricPoint {
+                name: "this.is.correct".to_owned(),
+                point: Point {
+                    interval: 1,
+                    value: 123_f64,
+                }
+            },
+            "It should be matched"
+        );
+    }
+
+    #[test]
+    fn test_metric_parse_incorrect_name() {
+        let s = "this\\.is./incorrect 1 123";
+        let metric_result = s.parse::<MetricPoint>();
+        assert!(
+            metric_result.is_err(),
+            "It({}) should not be parsed {:?}",
+            s,
+            metric_result.unwrap()
+        );
+    }
+
+    #[test]
+    fn test_metric_parse_incorrect_time() {
+        let metric_result = "this.is.correct a 123".parse::<MetricPoint>();
+        assert!(metric_result.is_err(), "It should not be parsed");
+    }
+
+    #[test]
+    fn test_metric_parse_incorrect_value() {
+        let metric_result = "this.is.correct 1 a123".parse::<MetricPoint>();
+        assert!(metric_result.is_err(), "It should not be parsed");
+    }
+
+    #[test]
+    fn test_metric_path_parse_correct() {
+        let metric_result = "this.is.correct.path".parse::<MetricPath>();
+        assert!(metric_result.is_ok(), "It should be parsed");
+
+        let metric = metric_result.unwrap();
+        assert_eq!(
+            metric,
+            Path::new("./this/is/correct/path.wsp").to_owned().into(),
+            "It should be matched"
+        );
+    }
+
+    #[test]
+    fn test_metric_path_parse_incorrect() {
+        let s = "this/.is.incorrect.path";
+        let metric_result = s.parse::<MetricPath>();
+        assert!(
+            metric_result.is_err(),
+            "It({}) should not be parsed {:?}",
+            s,
+            metric_result.unwrap()
+        );
+    }
+}
