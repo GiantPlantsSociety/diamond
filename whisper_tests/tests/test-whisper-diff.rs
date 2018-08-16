522babe4c9de6832268019dd8d7405c74d94e74a
Andrii Radyk <ander.ender@gmail.com>
2018-08-16 18:32:05 +0200

    add test for unfiltered values

--- whisper_tests/tests/test-whisper-diff.rs
+++ whisper_tests/tests/test-whisper-diff.rs
@@ -56,6 +56,7 @@ fn test_diff_simple_filtered() {
     let diff_points = whisper::diff::diff(&path1, &path2, true, now, now).unwrap();
 
     assert_eq!(diff_points[0].points, 1, "there should be 1 point");
+    assert_eq!(diff_points[0].total, 1, "there should be total 1");
 
     assert_eq!(
         diff_points[0].diffs[0],
@@ -63,7 +64,58 @@ fn test_diff_simple_filtered() {
             interval: now - 300,
             value1: Some(300.0),
             value2: Some(3000.0),
-        },
+        }
+    );
+
+}
+
+#[test]
+fn test_diff_simple_unfiltered() {
+    let temp_dir = get_temp_dir();
+
+    let path1 = get_file_path(&temp_dir, "diff1_1");
+    let path2 = get_file_path(&temp_dir, "diff2_2");
+
+    let now = 1528240800;
+
+    let _file1 = create_and_update_points(
+        &path1,
+        &[
+            Point { interval: now - 60, value: 60.0 },
+            Point { interval: now - 180, value: 180.0 },
+            Point { interval: now - 300, value: 300.0 },
+            Point { interval: now - 540, value: 540.0 },
+        ],
+        now,
+    );
+
+    let _file2 = create_and_update_points(
+        &path2,
+        &[
+            Point { interval: now - 120, value: 120.0 },
+            Point { interval: now - 300, value: 3000.0 },
+            Point { interval: now - 360, value: 360.0 },
+            Point { interval: now - 480, value: 480.0 },
+            Point { interval: now - 540, value: 540.0 },
+        ],
+        now,
+    );
+
+    let diff_points = whisper::diff::diff(&path1, &path2, false, now, now).unwrap();
+
+    assert_eq!(diff_points[0].points, 6, "there should be 6 points {:?}", &diff_points);
+    assert_eq!(diff_points[0].total, 7, "there should be total 7 - {:?}", &diff_points);
+
+    assert_eq!(
+        diff_points[0].diffs,
+        &[
+            DiffPoint { interval: now - 480, value1: None, value2: Some(480.0) },
+            DiffPoint { interval: now - 360, value1: None, value2: Some(360.0) },
+            DiffPoint { interval: now - 300, value1: Some(300.0), value2: Some(3000.0) },
+            DiffPoint { interval: now - 180, value1: Some(180.0), value2: None },
+            DiffPoint { interval: now - 120, value1: None, value2: Some(120.0) },
+            DiffPoint { interval: now - 60, value1: Some(60.0), value2: None },
+        ]
     );
 
 }
