d173b3abd13544fc54900e9668c22ba0ca1566f6
Andrii Radyk <ander.ender@gmail.com>
2018-10-07 22:12:47 +0200

    add new tests

--- whisper/tests/test-whisper-create.rs
+++ whisper/tests/test-whisper-create.rs
@@ -35,6 +35,61 @@ fn calling_help() {
 }
 
 #[test]
+fn calling_with_invalid_path() {
+    Command::cargo_bin(NAME)
+        .unwrap()
+        .args(&["invalid/path", "60:1440"])
+        .assert()
+        .code(1)
+        .stderr(predicate::str::contains("No such file or directory (os error 2)").from_utf8());
+}
+
+#[test]
+fn calling_with_invalid_method() {
+    let path = Builder::new()
+        .prefix("whisper")
+        .suffix("info.wsp")
+        .tempfile()
+        .unwrap()
+        .path()
+        .to_path_buf();
+
+    let error =
+        "error: Invalid value for '--aggregationMethod <aggregation_method>': Unsupported aggregation method 'unknown'";
+
+    Command::cargo_bin(NAME)
+        .unwrap()
+        .args(&[
+            path.to_str().unwrap(),
+            "60:1440",
+            "--aggregationMethod",
+            "unknown",
+        ]).assert()
+        .code(1)
+        .stderr(predicate::str::contains(error).from_utf8());
+}
+
+#[test]
+fn calling_with_invalid_xfactor() {
+    let path = Builder::new()
+        .prefix("whisper")
+        .suffix("info.wsp")
+        .tempfile()
+        .unwrap()
+        .path()
+        .to_path_buf();
+
+    let error = "error: Invalid value for '--xFilesFactor <x_files_factor>': invalid float literal";
+
+    Command::cargo_bin(NAME)
+        .unwrap()
+        .args(&[path.to_str().unwrap(), "60:1440", "--xFilesFactor", "nan"])
+        .assert()
+        .code(1)
+        .stderr(predicate::str::contains(error).from_utf8());
+}
+
+#[test]
 fn calling_creating_simple() {
     let filename = "info.wsp";
 
@@ -54,6 +109,29 @@ fn calling_creating_simple() {
         .stdout(predicate::str::contains("Created: ").from_utf8())
         .stdout(predicate::str::contains(path.to_str().unwrap()).from_utf8())
         .stdout(predicate::str::contains("(17308 bytes)").from_utf8())
+        .stderr("");
+}
+
+#[test]
+fn calling_creating_multiple_retention() {
+    let filename = "info.wsp";
+
+    let path = Builder::new()
+        .prefix("whisper")
+        .suffix(filename)
+        .tempdir()
+        .unwrap()
+        .path()
+        .to_path_buf();
+
+    Command::cargo_bin(NAME)
+        .unwrap()
+        .args(&[path.to_str().unwrap(), "60:1440", "300:1440"])
+        .assert()
+        .success()
+        .stdout(predicate::str::contains("Created: ").from_utf8())
+        .stdout(predicate::str::contains(path.to_str().unwrap()).from_utf8())
+        .stdout(predicate::str::contains("(34600 bytes)").from_utf8())
         .stderr("");
 }
 
--- whisper/tests/test-whisper-dump.rs
+++ whisper/tests/test-whisper-dump.rs
@@ -1,10 +1,17 @@
 #![cfg(test)]
+
 extern crate assert_cmd;
 extern crate predicates;
+extern crate tempfile;
+extern crate unindent;
 
 use assert_cmd::prelude::*;
 use predicates::prelude::*;
+use std::fs;
+use std::path::PathBuf;
 use std::process::Command;
+use tempfile::Builder;
+use unindent::unindent;
 
 const NAME: &str = "whisper-dump";
 
@@ -36,4 +43,160 @@ fn calling_with_invalid_path() {
         .args(&["invalid"])
         .assert()
         .code(1);
+}
+
+#[test]
+fn calling_as_plain() {
+    let filename = "dump.wsp";
+
+    let path = Builder::new()
+        .prefix("whisper")
+        .suffix(filename)
+        .tempdir()
+        .unwrap()
+        .path()
+        .to_path_buf();
+
+    let file_path = PathBuf::new().join("data").join(filename);
+
+    fs::copy(&file_path, &path).unwrap();
+
+    Command::cargo_bin(NAME)
+        .unwrap()
+        .args(&[path.to_str().unwrap()])
+        .assert()
+        .success()
+        .stdout(
+            predicate::str::contains(
+                unindent(
+                    "
+Meta data:
+  aggregation method: average
+  max retention: 600
+  xFilesFactor: 0.5",
+                ).as_str(),
+            ).from_utf8(),
+        ).stdout(
+            predicate::str::contains(
+                unindent(
+                    "
+Archive 0 info:
+  offset: 40
+  seconds per point: 60
+  points: 5
+  retention: 300
+  size: 60",
+                ).as_str(),
+            ).from_utf8(),
+        ).stdout(
+            predicate::str::contains(
+                unindent(
+                    "
+Archive 1 info:
+  offset: 100
+  seconds per point: 120
+  points: 5
+  retention: 600
+  size: 60",
+                ).as_str(),
+            ).from_utf8(),
+        ).stdout(
+            predicate::str::contains(
+                "
+Archive 0 data:
+0: 0,          0
+1: 0,          0
+2: 0,          0
+3: 0,          0
+4: 0,          0",
+            ).from_utf8(),
+        ).stdout(
+            predicate::str::contains(
+                "
+Archive 1 data:
+0: 0,          0
+1: 0,          0
+2: 0,          0
+3: 0,          0
+4: 0,          0",
+            ).from_utf8(),
+        ).stderr("");
+}
+
+#[test]
+fn calling_as_pretty() {
+    let filename = "dump.wsp";
+
+    let path = Builder::new()
+        .prefix("whisper")
+        .suffix(filename)
+        .tempdir()
+        .unwrap()
+        .path()
+        .to_path_buf();
+
+    let file_path = PathBuf::new().join("data").join(filename);
+
+    fs::copy(&file_path, &path).unwrap();
+
+    Command::cargo_bin(NAME)
+        .unwrap()
+        .args(&[path.to_str().unwrap(), "--pretty", "--time-format", "%c"])
+        .assert()
+        .success()
+        .stdout(
+            predicate::str::contains(
+                unindent(
+                    "
+Meta data:
+  aggregation method: average
+  max retention: 600
+  xFilesFactor: 0.5",
+                ).as_str(),
+            ).from_utf8(),
+        ).stdout(
+            predicate::str::contains(
+                unindent(
+                    "
+Archive 0 info:
+  offset: 40
+  seconds per point: 60
+  points: 5
+  retention: 300
+  size: 60",
+                ).as_str(),
+            ).from_utf8(),
+        ).stdout(
+            predicate::str::contains(
+                unindent(
+                    "
+Archive 1 info:
+  offset: 100
+  seconds per point: 120
+  points: 5
+  retention: 600
+  size: 60",
+                ).as_str(),
+            ).from_utf8(),
+        ).stdout(
+            predicate::str::contains(
+                "
+Archive 0 data:
+0: Thu Jan  1 00:00:00 1970,          0
+1: Thu Jan  1 00:00:00 1970,          0
+2: Thu Jan  1 00:00:00 1970,          0
+3: Thu Jan  1 00:00:00 1970,          0
+4: Thu Jan  1 00:00:00 1970,          0",
+            ).from_utf8(),
+        ).stdout(
+            predicate::str::contains(
+                "
+Archive 1 data:
+0: Thu Jan  1 00:00:00 1970,          0
+1: Thu Jan  1 00:00:00 1970,          0
+2: Thu Jan  1 00:00:00 1970,          0
+3: Thu Jan  1 00:00:00 1970,          0
+4: Thu Jan  1 00:00:00 1970,          0",
+            ).from_utf8(),
+        ).stderr("");
 }
