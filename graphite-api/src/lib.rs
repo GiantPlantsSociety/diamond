c953d95cb1ca13f934d5a897fbf33954c60c73b7
Andrey Kutejko <andrey.kutejko@sizmek.com>
2018-12-09 16:53:35 +0200

    cleanup

--- graphite-api/src/lib.rs
+++ graphite-api/src/lib.rs
@@ -1,6 +1,3 @@
-use structopt;
-use failure;
-
 pub mod application;
 pub mod find;
 pub mod opts;
--- whisper/tests/test-find-corrupt-whisper-files.rs
+++ whisper/tests/test-find-corrupt-whisper-files.rs
@@ -1,4 +1,3 @@
-#![cfg(test)]
 use assert_cmd::prelude::*;
 use predicates::prelude::*;
 use std::process::Command;
