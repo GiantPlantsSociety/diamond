1ef2349bbe6c0e1d5d65a5911440072b6faf6afd
Andrii Radyk <ander.ender@gmail.com>
2019-04-14 14:25:30 +0200

    add test for fetch_auto_points

--- whisper_tests/tests/test-whisper.rs
+++ whisper_tests/tests/test-whisper.rs
@@ -4,7 +4,8 @@ use whisper::builder::WhisperBuilder;
 use whisper::interval::Interval;
 use whisper::point::Point;
 use whisper::retention::Retention;
-use whisper_tests::{get_temp_dir, get_file_path};
+use whisper_tests::*;
+use whisper::ArchiveData;
 
 #[test]
 fn whisper_suggest_archive() -> Result<(), Error> {
@@ -78,6 +79,62 @@ fn whisper_suggest_archive() -> Result<(), Error> {
         ),
         None
     );
+
+    Ok(())
+}
+
+#[test]
+fn whisper_fetch_auto_points() -> Result<(), Error> {
+    let temp_dir = get_temp_dir();
+    let path = get_file_path(&temp_dir, "fetch_auto_points");
+
+    let now = 1528240800;
+
+    let mut file = create_and_update_points(
+        &path,
+        &[
+            Point {
+                interval: now - 60,
+                value: 60.0,
+            },
+            Point {
+                interval: now - 180,
+                value: 180.0,
+            },
+            Point {
+                interval: now - 300,
+                value: 300.0,
+            },
+            Point {
+                interval: now - 540,
+                value: 540.0,
+            },
+        ],
+        now,
+    )?;
+
+    let interval = Interval::new(now - 540, now - 60).map_err(err_msg)?;
+
+    let data = ArchiveData {
+        from_interval: 1528240260,
+        until_interval: 1528240740,
+        step: 60,
+        values: vec![
+            Some(540.0),
+            None,
+            None,
+            None,
+            Some(300.0),
+            None,
+            Some(180.0),
+            None,
+        ],
+    };
+
+    assert_eq!(file.fetch_auto_points(interval, now)?, data);
+
+    let interval2 = Interval::new(now - 3000, now - 900).map_err(err_msg)?;
+    assert!(file.fetch_auto_points(interval2, now).is_err());
 
     Ok(())
 }
