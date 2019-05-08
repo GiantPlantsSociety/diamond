c94f63a2762110cb7f1ae746e365494dc854b967
Andrii Radyk <ander.ender@gmail.com>
2019-05-08 20:24:26 +0200

    initial udp server implementation

--- carbon/src/bin/carbon-server.rs
+++ carbon/src/bin/carbon-server.rs
@@ -11,7 +11,7 @@ use std::time::{SystemTime, UNIX_EPOCH};
 use structopt::StructOpt;
 use tokio::codec::Framed;
 use tokio::codec::LinesCodec;
-use tokio::net::TcpListener;
+use tokio::net::{TcpListener, UdpFramed, UdpSocket};
 use tokio::prelude::*;
 
 #[derive(Debug, StructOpt)]
@@ -34,15 +34,20 @@ fn run(args: Args) -> Result<(), Error> {
 
     let settings = Settings::new(args.config)?;
 
-    let addr = format!("{0}:{1}", &settings.tcp.host, settings.tcp.port).parse()?;
-    let listener = TcpListener::bind(&addr)?;
-    let config = Arc::new(settings);
+    let tcp_addr = format!("{0}:{1}", &settings.tcp.host, settings.tcp.port).parse()?;
+    let udp_addr = format!("{0}:{1}", &settings.udp.host, settings.udp.port).parse()?;
 
-    let server = listener
+    let tcp_listener = TcpListener::bind(&tcp_addr)?;
+    let udp_listener = UdpSocket::bind(&udp_addr)?;
+
+    let config_tcp = Arc::new(settings);
+    let config_udp = config_tcp.clone();
+
+    let server = tcp_listener
         .incoming()
         .for_each(move |sock| {
             let framed_sock = Framed::new(sock, LinesCodec::new());
-            let conf = config.clone();
+            let conf = config_tcp.clone();
 
             framed_sock.for_each(move |line| {
                 let now = SystemTime::now()
@@ -58,8 +63,31 @@ fn run(args: Args) -> Result<(), Error> {
             eprintln!("accept error = {:?}", err);
         });
 
-    println!("server running on {}", addr);
-    tokio::run(server);
+    println!("server running on tcp {}", tcp_addr);
+
+    let udp_server = UdpFramed::new(udp_listener, LinesCodec::new())
+        .for_each(move |(line, _)| {
+            let conf = config_udp.clone();
+            let now = SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .unwrap()
+                .as_secs() as u32;
+            line_update(&line, &conf.db_path, &conf.whisper, now)
+                .map_err(|e| io::Error::new(io::ErrorKind::Other, e.to_string()))?;
+            Ok(())
+        })
+        .map_err(|err| {
+            eprintln!("accept error = {:?}", err);
+        });
+
+    println!("server running on udp {}", udp_addr);
+
+    tokio::run({
+        server
+            .join(udp_server)
+            .map(|_| ())
+            .map_err(|e| println!("error = {:?}", e))
+    });
 
     Ok(())
 }
