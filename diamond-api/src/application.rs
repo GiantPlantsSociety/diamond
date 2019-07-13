36b9adbb871359050c1cf5a4d23135502ead922f
Andrey Kutejko <andy128k@gmail.com>
2019-07-13 20:18:29 +0200

    extract app config

---
+++ diamond-api/src/application.rs
@@ -0,0 +1,14 @@
+use crate::find::*;
+use crate::opts::*;
+use crate::render::*;
+use actix_web::web::{ServiceConfig, resource};
+
+pub fn app_config(args: Args) -> impl Fn(&mut ServiceConfig) {
+    move |config: &mut ServiceConfig| {
+        config
+            .data(args.clone())
+            .service(resource("/render").to_async(render_handler))
+            .service(resource("/metrics/find").to_async(find_handler))
+            .service(resource("/metrics").to_async(find_handler));
+    }
+}
