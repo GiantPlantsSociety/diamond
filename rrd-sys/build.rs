342b4ec4bd18a6becb59d6e4b8b1814daeebe5fe
Andrey Kutejko <andy128k@gmail.com>
2024-06-04 20:08:45 +0200

    Use system-deps crate to look for librrd

--- rrd-sys/build.rs
+++ rrd-sys/build.rs
@@ -1,5 +1,3 @@
 fn main() {
-    #[cfg(target_os = "freebsd")]
-    println!("cargo:rustc-link-search=native=/usr/local/lib");
-    println!("cargo:rustc-link-lib=rrd");
+    system_deps::Config::new().probe().unwrap();
 }
--- rrd-sys/src/tests.rs
+++ rrd-sys/src/tests.rs
@@ -1,314 +1,66 @@
 use super::*;
+use std::mem::{align_of, offset_of, size_of};
 
 #[test]
 fn bindgen_test_layout_rrd_file_t() {
-    assert_eq!(
-        ::std::mem::size_of::<rrd_file_t>(),
-        40usize,
-        "Size of rrd_file_t"
-    );
-    assert_eq!(
-        ::std::mem::align_of::<rrd_file_t>(),
-        8usize,
-        "Alignment of rrd_file_t"
-    );
-    assert_eq!(
-        unsafe { &(*(::std::ptr::null::<rrd_file_t>())).header_len as *const _ as usize },
-        0usize,
-        concat!(
-            "Offset of field: ",
-            stringify!(rrd_file_t),
-            "::",
-            stringify!(header_len)
-        )
-    );
-    assert_eq!(
-        unsafe { &(*(::std::ptr::null::<rrd_file_t>())).file_len as *const _ as usize },
-        8usize,
-        concat!(
-            "Offset of field: ",
-            stringify!(rrd_file_t),
-            "::",
-            stringify!(file_len)
-        )
-    );
-    assert_eq!(
-        unsafe { &(*(::std::ptr::null::<rrd_file_t>())).pos as *const _ as usize },
-        16usize,
-        concat!(
-            "Offset of field: ",
-            stringify!(rrd_file_t),
-            "::",
-            stringify!(pos)
-        )
-    );
-    assert_eq!(
-        unsafe { &(*(::std::ptr::null::<rrd_file_t>())).pvt as *const _ as usize },
-        24usize,
-        concat!(
-            "Offset of field: ",
-            stringify!(rrd_file_t),
-            "::",
-            stringify!(pvt)
-        )
-    );
-    assert_eq!(
-        unsafe { &(*(::std::ptr::null::<rrd_file_t>())).rrd as *const _ as usize },
-        32usize,
-        concat!(
-            "Offset of field: ",
-            stringify!(rrd_file_t),
-            "::",
-            stringify!(rrd)
-        )
-    );
+    assert_eq!(size_of::<rrd_file_t>(), 40);
+    assert_eq!(align_of::<rrd_file_t>(), 8);
+    assert_eq!(offset_of!(rrd_file_t, header_len), 0,);
+    assert_eq!(offset_of!(rrd_file_t, file_len), 8,);
+    assert_eq!(offset_of!(rrd_file_t, pos), 16,);
+    assert_eq!(offset_of!(rrd_file_t, pvt), 24,);
+    assert_eq!(offset_of!(rrd_file_t, rrd), 32);
 }
 
 #[test]
 fn bindgen_test_layout_rrd_simple_file_t() {
-    assert_eq!(
-        ::std::mem::size_of::<rrd_simple_file_t>(),
-        4usize,
-        "Size of rrd_simple_file_t"
-    );
-    assert_eq!(
-        ::std::mem::align_of::<rrd_simple_file_t>(),
-        4usize,
-        "Alignment of rrd_simple_file_t"
-    );
-    assert_eq!(
-        unsafe { &(*(::std::ptr::null::<rrd_simple_file_t>())).fd as *const _ as usize },
-        0usize,
-        "Offset of field rrd_simple_file_t::fd"
-    );
+    assert_eq!(size_of::<rrd_simple_file_t>(), 4,);
+    assert_eq!(align_of::<rrd_simple_file_t>(), 4,);
+    assert_eq!(offset_of!(rrd_simple_file_t, fd), 0,);
 }
 
 #[test]
 fn bindgen_test_layout_rrd_blob_t() {
-    assert_eq!(
-        ::std::mem::size_of::<rrd_blob_t>(),
-        16usize,
-        concat!("Size of: ", stringify!(rrd_blob_t))
-    );
-    assert_eq!(
-        ::std::mem::align_of::<rrd_blob_t>(),
-        8usize,
-        concat!("Alignment of ", stringify!(rrd_blob_t))
-    );
-    assert_eq!(
-        unsafe { &(*(::std::ptr::null::<rrd_blob_t>())).size as *const _ as usize },
-        0usize,
-        concat!(
-            "Offset of field: ",
-            stringify!(rrd_blob_t),
-            "::",
-            stringify!(size)
-        )
-    );
-    assert_eq!(
-        unsafe { &(*(::std::ptr::null::<rrd_blob_t>())).ptr as *const _ as usize },
-        8usize,
-        concat!(
-            "Offset of field: ",
-            stringify!(rrd_blob_t),
-            "::",
-            stringify!(ptr)
-        )
-    );
+    assert_eq!(size_of::<rrd_blob_t>(), 16);
+    assert_eq!(align_of::<rrd_blob_t>(), 8);
+    assert_eq!(offset_of!(rrd_blob_t, size), 0);
+    assert_eq!(offset_of!(rrd_blob_t, ptr), 8);
 }
 
 #[test]
 fn bindgen_test_layout_rrd_infoval() {
-    assert_eq!(
-        ::std::mem::size_of::<rrd_infoval>(),
-        16usize,
-        concat!("Size of: ", stringify!(rrd_infoval))
-    );
-    assert_eq!(
-        ::std::mem::align_of::<rrd_infoval>(),
-        8usize,
-        concat!("Alignment of ", stringify!(rrd_infoval))
-    );
-    assert_eq!(
-        unsafe { &(*(::std::ptr::null::<rrd_infoval>())).u_cnt as *const _ as usize },
-        0usize,
-        concat!(
-            "Offset of field: ",
-            stringify!(rrd_infoval),
-            "::",
-            stringify!(u_cnt)
-        )
-    );
-    assert_eq!(
-        unsafe { &(*(::std::ptr::null::<rrd_infoval>())).u_val as *const _ as usize },
-        0usize,
-        concat!(
-            "Offset of field: ",
-            stringify!(rrd_infoval),
-            "::",
-            stringify!(u_val)
-        )
-    );
-    assert_eq!(
-        unsafe { &(*(::std::ptr::null::<rrd_infoval>())).u_str as *const _ as usize },
-        0usize,
-        concat!(
-            "Offset of field: ",
-            stringify!(rrd_infoval),
-            "::",
-            stringify!(u_str)
-        )
-    );
-    assert_eq!(
-        unsafe { &(*(::std::ptr::null::<rrd_infoval>())).u_int as *const _ as usize },
-        0usize,
-        concat!(
-            "Offset of field: ",
-            stringify!(rrd_infoval),
-            "::",
-            stringify!(u_int)
-        )
-    );
-    assert_eq!(
-        unsafe { &(*(::std::ptr::null::<rrd_infoval>())).u_blo as *const _ as usize },
-        0usize,
-        concat!(
-            "Offset of field: ",
-            stringify!(rrd_infoval),
-            "::",
-            stringify!(u_blo)
-        )
-    );
+    assert_eq!(size_of::<rrd_infoval>(), 16);
+    assert_eq!(align_of::<rrd_infoval>(), 8);
+    assert_eq!(offset_of!(rrd_infoval, u_cnt), 0);
+    assert_eq!(offset_of!(rrd_infoval, u_val), 0);
+    assert_eq!(offset_of!(rrd_infoval, u_str), 0);
+    assert_eq!(offset_of!(rrd_infoval, u_int), 0);
+    assert_eq!(offset_of!(rrd_infoval, u_blo), 0);
 }
 
 #[test]
 fn bindgen_test_layout_rrd_info_t() {
-    assert_eq!(
-        ::std::mem::size_of::<rrd_info_t>(),
-        40usize,
-        concat!("Size of: ", stringify!(rrd_info_t))
-    );
-    assert_eq!(
-        ::std::mem::align_of::<rrd_info_t>(),
-        8usize,
-        concat!("Alignment of ", stringify!(rrd_info_t))
-    );
-    assert_eq!(
-        unsafe { &(*(::std::ptr::null::<rrd_info_t>())).key as *const _ as usize },
-        0usize,
-        concat!(
-            "Offset of field: ",
-            stringify!(rrd_info_t),
-            "::",
-            stringify!(key)
-        )
-    );
-    assert_eq!(
-        unsafe { &(*(::std::ptr::null::<rrd_info_t>())).type_ as *const _ as usize },
-        8usize,
-        concat!(
-            "Offset of field: ",
-            stringify!(rrd_info_t),
-            "::",
-            stringify!(type_)
-        )
-    );
-    assert_eq!(
-        unsafe { &(*(::std::ptr::null::<rrd_info_t>())).value as *const _ as usize },
-        16usize,
-        concat!(
-            "Offset of field: ",
-            stringify!(rrd_info_t),
-            "::",
-            stringify!(value)
-        )
-    );
-    assert_eq!(
-        unsafe { &(*(::std::ptr::null::<rrd_info_t>())).next as *const _ as usize },
-        32usize,
-        concat!(
-            "Offset of field: ",
-            stringify!(rrd_info_t),
-            "::",
-            stringify!(next)
-        )
-    );
+    assert_eq!(size_of::<rrd_info_t>(), 40,);
+    assert_eq!(align_of::<rrd_info_t>(), 8);
+    assert_eq!(offset_of!(rrd_info_t, key), 0);
+    assert_eq!(offset_of!(rrd_info_t, type_), 8);
+    assert_eq!(offset_of!(rrd_info_t, value), 16);
+    assert_eq!(offset_of!(rrd_info_t, next), 32);
 }
 
 #[test]
 fn bindgen_test_layout_rrd_time_value() {
-    assert_eq!(
-        ::std::mem::size_of::<rrd_time_value>(),
-        72usize,
-        concat!("Size of: ", stringify!(rrd_time_value))
-    );
-    assert_eq!(
-        ::std::mem::align_of::<rrd_time_value>(),
-        8usize,
-        concat!("Alignment of ", stringify!(rrd_time_value))
-    );
-    assert_eq!(
-        unsafe { &(*(::std::ptr::null::<rrd_time_value>())).type_ as *const _ as usize },
-        0usize,
-        concat!(
-            "Offset of field: ",
-            stringify!(rrd_time_value),
-            "::",
-            stringify!(type_)
-        )
-    );
-    assert_eq!(
-        unsafe { &(*(::std::ptr::null::<rrd_time_value>())).offset as *const _ as usize },
-        8usize,
-        concat!(
-            "Offset of field: ",
-            stringify!(rrd_time_value),
-            "::",
-            stringify!(offset)
-        )
-    );
-    assert_eq!(
-        unsafe { &(*(::std::ptr::null::<rrd_time_value>())).tm as *const _ as usize },
-        16usize,
-        concat!(
-            "Offset of field: ",
-            stringify!(rrd_time_value),
-            "::",
-            stringify!(tm)
-        )
-    );
+    assert_eq!(size_of::<rrd_time_value>(), 72);
+    assert_eq!(align_of::<rrd_time_value>(), 8);
+    assert_eq!(offset_of!(rrd_time_value, type_), 0);
+    assert_eq!(offset_of!(rrd_time_value, offset), 8);
+    assert_eq!(offset_of!(rrd_time_value, tm), 16);
 }
 
 #[test]
 fn bindgen_test_layout_rrd_context() {
-    assert_eq!(
-        ::std::mem::size_of::<rrd_context>(),
-        4352usize,
-        concat!("Size of: ", stringify!(rrd_context))
-    );
-    assert_eq!(
-        ::std::mem::align_of::<rrd_context>(),
-        1usize,
-        concat!("Alignment of ", stringify!(rrd_context))
-    );
-    assert_eq!(
-        unsafe { &(*(::std::ptr::null::<rrd_context>())).lib_errstr as *const _ as usize },
-        0usize,
-        concat!(
-            "Offset of field: ",
-            stringify!(rrd_context),
-            "::",
-            stringify!(lib_errstr)
-        )
-    );
-    assert_eq!(
-        unsafe { &(*(::std::ptr::null::<rrd_context>())).rrd_error as *const _ as usize },
-        256usize,
-        concat!(
-            "Offset of field: ",
-            stringify!(rrd_context),
-            "::",
-            stringify!(rrd_error)
-        )
-    );
+    assert_eq!(size_of::<rrd_context>(), 4352);
+    assert_eq!(align_of::<rrd_context>(), 1);
+    assert_eq!(offset_of!(rrd_context, lib_errstr), 0);
+    assert_eq!(offset_of!(rrd_context, rrd_error), 256);
 }
